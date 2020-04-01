298e3d508271a0d2cf08ae29f06935eaecdecb88
axtros <axtros@gmail.com>
2020-04-01 14:56:45 +0200

    Fix.

--- js/ts/test.ts
+++ js/ts/test.ts
@@ -1,5 +1,9 @@
 //typescript demo
 
+//első lépésben a nodejs-t kell telepíteni, illetve a windows a path-ben hozzá kell adni az elérésekhez az npm managert: nodejs\npm.cmd
+//C:\Users\Axtros\AppData\Roaming\npm\tsserver -> C:\Users\Axtros\AppData\Roaming\npm\node_modules\typescript\bin\tsserver
+//C:\Users\Axtros\AppData\Roaming\npm\tsc -> C:\Users\Axtros\AppData\Roaming\npm\node_modules\typescript\bin\tsc
+
 //Typescript telepítése VSCode alá, getting started
 //https://code.visualstudio.com/docs/typescript/typescript-tutorial
 //https://www.digitalocean.com/community/tutorials/how-to-work-with-typescript-in-visual-studio-code
